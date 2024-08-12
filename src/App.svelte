7874b04b918cfed9c7649d76acda960c4e456402
jagaoji <176724177+jagaoji@users.noreply.github.com>
2024-08-12 21:53:16 +0900

    emojiを単純に一覧表示

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,11 @@
 <script lang="ts">
+  import dataByEmoji from "unicode-emoji-json/data-by-emoji.json";
 </script>
 
-<div class="bg-red underline">aeae</div>
-<!-- A basic anchor icon from Phosphor icons -->
-<div class="i-ph-anchor-simple-thin" />
-<!-- An orange alarm from Material Design Icons -->
-<div class="i-mdi-alarm text-orange-400" />
-<!-- A large Vue logo -->
-<div class="i-logos-vue text-3xl" />
-<!-- Sun in light mode, Moon in dark mode, from Carbon -->
-<button class="i-carbon-sun dark:i-carbon-moon" />
-<!-- Twemoji of laugh, turns to tear on hovering -->
-<div
-  class="i-twemoji-grinning-face-with-smiling-eyes hover:i-twemoji-face-with-tears-of-joy"
-/>
+<div class="flex flex-wrap">
+  {#each Object.entries(dataByEmoji) as [emoji, { name, group, skin_tone_support, unicode_version, ...rest }]}
+    <div class="noto-color-emoji-regular text-3xl">
+      {emoji}
+    </div>
+  {/each}
+</div>
